41de71a523b52be07fe4a93a92c0b33d65a5db3e
hs-krispy <0864hs@naver.com>
2020-04-14 20:32:35 +0900

    Modify FIFO

--- lab1_sched.cpp
+++ lab1_sched.cpp
@@ -7,45 +7,6 @@
 #include <string.h>
 #include <stdio.h>
 using namespace std;
-<<<<<<< HEAD
-typedef struct process {
-    char processName; // 프로세스 이름, priority
-    int serviceTime; // 동작시간
-    int arriveTime; // 도착시간
-    int waitTime; // 대기시간
-} process;
-void SortByArriveTime(process *p, int n) { // 도착시간이 빠른 것 부터, 정렬
-    process temp;
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < i; j++) {
-            if(p[j].arriveTime > p[j + 1].arriveTime) {
-                Swap(p[j], p[j + 1]);
-            } else if(p[j].arriveTime == p[j + 1].arriveTime && p[j].processNumber > p[j + 1].processNumber) { // 번호가 빠른 것 부터
-                Swap(p[j], p[j + 1]);
-            }
-        }
-    }
-}
-void FIFO(process *ps, int count)
-{
-    int Turnaround[n] = {p[0].serviceTime, };
-    int Wait[n] = {0, };
-    printf("Process name : %c, Turnaround time : %d, Wait time : %d\n", p[0].processName, Turnaround[0], Wait[0])
-    for(int i=1;i<n;i++)
-    {
-        Turnaround[i] = Turnaround[i-1] + p[i].serviceTime;
-        Wait[i] = Turnaround[i-1] - p[i].arriveTime;
-        printf("Process name : %c, Turnaround time : %d, Wait time : %d\n", p[i].processName, Turnaround[i], Wait[i])
-    }
-}
-int main() {
-    int count;
-    cin >> count; // 프로세스 개수
-    process ps[count];
-    for(int i = 0; i < count; i++)//프로세스 정보 입력
-        scanf("Process name : %c, Arrive time : %d, Service time : %d", &ps[i].processName, &ps[i].arriveTime, &ps[i].serviceTime);
-    SortByArriveTime(ps, count);
-=======
 
 /* define process struct */
 struct process {
@@ -74,8 +35,6 @@ vector<int> turnaround(5, 0); // 5 processes
 vector<int> pWait(5, -1); // 5 processes
 /* end of calculate and print */
 
-
-
 void SetInit() {
     v[0].arriveTime = 0, v[0].serviceTime = 3;
     v[1].arriveTime = 2, v[1].serviceTime = 6;
@@ -83,6 +42,7 @@ void SetInit() {
     v[3].arriveTime = 6, v[3].serviceTime = 5;
     v[4].arriveTime = 8, v[4].serviceTime = 2;
 }
+
 // scheduling table ■ □
 void Print() {
     char task[5] = {'A', 'B', 'C', 'D', 'E'};
@@ -164,8 +124,16 @@ void getPerformance(vector<process> p)
     }
     cout<<"Average turnaroundTime : "<<avgTurnaround<<" Average waitTime : "<<avgWait<<"\n";
 } // print Performance
+void FIFO(vector<process> p)
+{
+    queue<process> q;
+    q.push(p[0]);
+    for(int i = 0; i < 5; i++)
+        result.push({p[i].processName, p[i].serviceTime});
+}
 int main() {
     SetInit();
-    return 0;
->>>>>>> feature/common
+    FIFO(v);
+    getPerformance(v);
+    Print();
 }
