1a042a0c28f0e6f74b6a982d03e5617e7de1a4bc
hs-krispy <0864hs@naver.com>
2020-04-16 14:41:38 +0900

    Apply GCD, LCM

--- lab1_sched.cpp
+++ lab1_sched.cpp
@@ -18,29 +18,33 @@ struct process {
     /*--------------*/
 };
 void Stride(vector<process> p) {
-    int TC = 100; // 얼마나 반복할건지.
-    queue<char> q;
-    int commonMulti = 1;
+    int TC = 100; // 반복 횟수
+    int commonMulti = 0; // 최소공배수가 들어갈 변수
     int minIdx = 0;
-    for(int i = 0; i < p.size(); i++) {
-        commonMulti *= p[i].ticket;
+    int temp = 0;
+    for(int i = 2; i < p.size(); i++) { // 각 ticket값들의 최소공배수를 구함
+        if(i == 2)
+            temp = LCM(p[0].ticket, p[1].ticket);
+        commonMulti = LCM(p[i].ticket, temp);
+        temp = commonMulti;
         p[i].passValue = 0;
     }
-    for(int i = 0; i < p.size(); i++) {
+    for(int i = 0; i < p.size(); i++) { // stride값 설정
         p[i].stride = commonMulti / p[i].ticket;
     }
-    for(int i = i; i < TC; i++) {
+    for(int i = 0; i < TC; i++) {
         int min = INT_MIN;
         for(int j = 0; j < p.size(); j++) {
-            if(min > p[j].passValue) {
+            if(min > p[j].passValue) { // passValue가 가장 작은 프로세스부터 실행
                 min = p[j].passValue;
                 minIdx = j;
             }
         }
-        q.push(p[minIdx].processName);
-        p[minIdx].passValue += p[minIdx].stride;
+        result.push(p[minIdx].processName, 1);
+        p[minIdx].passValue += p[minIdx].stride; // stride만큼 passValue 증가
     }
 }
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
